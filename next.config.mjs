5b5fb4ba84d7d78504100dcf41768afd228723e0
dqwud <125872651+dqwud@users.noreply.github.com>
2024-06-02 18:11:32 +0200

    Dawud

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    image:{
+        domains: [
+            "vsnzljlmozgbceifoctv.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
