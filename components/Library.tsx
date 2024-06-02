415f47104cb5e0b8649e6a6a0eb2ee4f4ce02700
Safet <safet.imamovic.22@size.ba>
2024-06-02 13:31:54 +0200

    dodana mogucnost uploadovanja pjesama i slika na bucket i INSERT INTO songs tabelu

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,11 +2,24 @@
 
 import {TbPlaylist} from "react-icons/tb";
 import {AiOutlinePlus} from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
-    const onClick = () => 
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
+    const onClick = () => {
+        if (!user)
         {
-            //Handle Upload Later
+            return authModal.onOpen();
+        }
+
+        //todo check for subscription
+
+        return uploadModal.onOpen();
     };
     return ( 
         <div className="flex flex-col">
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 import { useEffect, useState } from "react";
 
 
@@ -22,6 +23,7 @@ const ModalProvider = () =>
     return ( 
         <>
             <AuthModal />
+            <UploadModal />
         </>
     );
 }
